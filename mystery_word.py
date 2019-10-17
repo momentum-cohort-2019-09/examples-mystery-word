2796391071a758adcb4c0e27aedd788369e3056f
Clinton Dreisbach <clinton@dreisbach.us>
2019-10-17 08:29:10 -0400

    Update to Mystery Word from 2019-10-16 PM

--- mystery_word.py
+++ mystery_word.py
@@ -19,3 +19,65 @@ def is_valid_letter(user_input):
 
 def has_been_guessed(letter, correct_guesses, incorrect_guesses):
     return letter in correct_guesses + incorrect_guesses
+
+
+class Game:
+
+    def __init__(self, word):
+        self.word = word
+        self.correct_guesses = []
+        self.incorrect_guesses = []
+        self.max_incorrect_guesses = 8
+
+    def is_game_over(self):
+        if self.have_all_letters_been_guessed():
+            return True
+        return len(self.incorrect_guesses) >= self.max_incorrect_guesses
+
+    def have_all_letters_been_guessed(self):
+        # alternative
+        # return all([letter in self.correct_guesses for letter in self.word])
+
+        for letter in self.word:
+            if letter not in self.correct_guesses:
+                return False
+        return True
+
+    def add_guess(self, guess):
+        """Given a guessed letter, add it to correct or incorrect guesses."""
+        guess = guess.lower()
+        if guess in self.word.lower():
+            self.correct_guesses.append(guess)
+        else:
+            self.incorrect_guesses.append(guess)
+
+    def get_user_guess(self):
+        while True:
+            guess = input("What is your guess? ")
+            if is_valid_letter(guess):
+                return guess
+            print("That's not a valid answer!")
+
+    def run(self):
+        print("Welcome to Mystery Word!")
+
+        while not self.is_game_over():
+            print(
+                f"You have {self.max_incorrect_guesses - len(self.incorrect_guesses)} guesses left."
+            )
+            print(display_word(self.word, self.correct_guesses))
+            guess = self.get_user_guess()
+            self.add_guess(guess)
+            if self.is_game_over():
+                if self.have_all_letters_been_guessed():
+                    print("You won!")
+                else:
+                    print("You are the loser!")
+
+    def __repr__(self):
+        return f"<Game word={self.word}>"
+
+
+if __name__ == "__main__":
+    game = Game("beluga")
+    game.run()
--- mystery_word_test.py
+++ mystery_word_test.py
@@ -51,3 +51,57 @@ def test_has_been_guessed():
 def test_has_been_guessed_false():
     assert not m.has_been_guessed(
         "A", correct_guesses=["T", "Z"], incorrect_guesses=["D"])
+
+
+def test_game_can_be_made():
+    game = m.Game(word="beluga")
+    assert game.word == "beluga"
+    assert len(game.correct_guesses) == 0
+    assert len(game.incorrect_guesses) == 0
+
+
+def test_game_is_not_over_if_no_guesses():
+    game = m.Game(word="beluga")
+    assert not game.is_game_over()
+
+
+def test_game_over_from_incorrect_guesses():
+    game = m.Game(word="beluga")
+    game.incorrect_guesses = ["c", "d", "f", "h", "i", "j", "k", "m"]
+    assert game.is_game_over()
+
+    game.incorrect_guesses.append("z")
+    assert game.is_game_over()
+
+
+def test_game_over_from_correct_guesses():
+    game = m.Game(word="beluga")
+    game.correct_guesses = ["b", "e", "l", "u", "g", "a"]
+    assert game.is_game_over()
+
+
+def test_have_all_letters_been_guessed_true():
+    game = m.Game(word="beluga")
+    game.correct_guesses = ["b", "e", "l", "u", "g", "a"]
+    assert game.have_all_letters_been_guessed()
+
+
+def test_add_guess_adds_correct_guess():
+    game = m.Game(word="beluga")
+    game.add_guess("b")
+    assert len(game.correct_guesses) == 1
+    assert len(game.incorrect_guesses) == 0
+
+
+def test_add_guess_adds_correct_guess_case_insensitive():
+    game = m.Game(word="beluga")
+    game.add_guess("B")
+    assert len(game.correct_guesses) == 1
+    assert len(game.incorrect_guesses) == 0
+
+
+def test_add_guess_adds_incorrect_guess():
+    game = m.Game(word="beluga")
+    game.add_guess("d")
+    assert len(game.correct_guesses) == 0
+    assert len(game.incorrect_guesses) == 1
